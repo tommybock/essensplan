b8ab4c515e80dafcffa11243ec780886f809790a
tommybock <40018522+tommybock@users.noreply.github.com>
2024-06-27 14:24:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dishes = document.querySelectorAll('.dish');
+function toggleRecipe(recipeId) {
+    const recipeDetails = document.getElementById(recipeId);
+    recipeDetails.classList.toggle('active');
+}
 
-    dishes.forEach(dish => {
-        dish.addEventListener('click', function() {
-            const recipeId = this.getAttribute('data-recipe-id');
-            const recipe = document.getElementById(recipeId);
 
-            if (recipe.style.display === 'none' || recipe.style.display === '') {
-                recipe.style.display = 'block';
-            } else {
-                recipe.style.display = 'none';
-            }
-        });
-    });
-});
